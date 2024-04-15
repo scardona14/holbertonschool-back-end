8d2c0aaaa9701fa7b47562e29b0c7afdee593077
scardona14 <cardanasean123@gmail.com>
2024-04-15 11:12:00 -0400

    Using my Python script to export data in the JSON format

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/bin/bash/python3
+"""
+Given a specific API, returns information about the user's TODO list progress
+"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(url)
+    user = response.json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1])
+    response = requests.get(url)
+    todos = response.json()
+    with open("{}.json".format(argv[1]), "w") as jsonfile:
+        json.dump({argv[1]: [{"task": todo.get("title"),
+                              "completed": todo.get("completed"),
+                              "username": user.get("username")} for todo in todos]}, jsonfile)
---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,21 @@
+#!/bin/bash/python3
+"""
+Given a specific API, returns information about the user's TODO list progress
+"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url)
+    users = response.json()
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    todos = response.json()
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({user.get("id"): [{"task": todo.get("title"),
+                                     "completed": todo.get("completed"),
+                                     "username": user.get("username")} for todo in todos
+                                    if user.get("id") == todo.get("userId")]
+                  for user in users}, jsonfile)

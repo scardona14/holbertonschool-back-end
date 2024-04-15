1e22261619cc78d1f32429786d9f13cbfaca442a
scardona14 <cardanasean123@gmail.com>
2024-04-15 10:57:53 -0400

    Specific API that returns information about the user

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+"""
+Given a specific API, returns information about the user's TODO list progress
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(url)
+    user = response.json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1])
+    response = requests.get(url)
+    todos = response.json()
+    completed = [todo for todo in todos if todo.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed), len(todos)))
+    [print("\t {}".format(todo.get("title"))) for todo in completed]

a0f7c96a90ad5feb10c1e75d9b64d234465daf4b
scardona14 <cardanasean123@gmail.com>
2024-04-16 12:40:58 -0400

    Executable file

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Given a specific API, returns info about the user's TODO list progress
+Given a specific API, returns information about the user's TODO list progress
 """
 import csv
 import requests
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     response = requests.get(url)
     user = response.json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format
-    (argv[1])
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv
+                                                                        [1])
     response = requests.get(url)
     todos = response.json()
     with open("{}.csv".format(argv[1]), "w") as csvfile:

f5a7ee73fecbc51733650856f496f9b95e7f7cb5
scardona14 <cardanasean123@gmail.com>
2024-04-16 11:26:58 -0400

    Added a new line at end of file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     response = requests.get(url)
     user = response.json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1])
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format
+    (argv[1])
     response = requests.get(url)
     todos = response.json()
     with open("{}.json".format(argv[1]), "w") as jsonfile:

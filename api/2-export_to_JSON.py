fd655014c60edcfb51c290fff79d0a512f71938b
scardona14 <cardanasean123@gmail.com>
2024-04-16 12:49:37 -0400

    Fixed pycodestyle

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,16 +2,16 @@
 """
 Given a specific API, returns information about the user's TODO list progress
 """
-import json
 import requests
 from sys import argv
+import json
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     response = requests.get(url)
     user = response.json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format
-    (argv[1])
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv
+                                                                        [1])
     response = requests.get(url)
     todos = response.json()
     with open("{}.json".format(argv[1]), "w") as jsonfile:

7e63218773d98f5f68c30239b290dc084463597e
scardona14 <cardanasean123@gmail.com>
2024-04-16 11:56:29 -0400

    Added docimentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,21 +2,36 @@
 """
 Given a specific API, returns information about the user's TODO list progress
 """
-import json
+
 import requests
 from sys import argv
 
+def get_todo_list_progress(user_id):
+    """
+    Retrieves the user's TODO list progress from a specific API.
+    """
+    # Retrieve user information
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user_response = requests.get(user_url)
+    user = user_response.json()
+    
+    # Retrieve user's TODO list
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    todo_response = requests.get(todo_url)
+    todos = todo_response.json()
+    
+    # Filter completed tasks
+    completed = [todo for todo in todos if todo.get("completed")]
+    
+    # Print user's progress
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                           len(completed), len(todos)))
+    for todo in completed:
+        print("\t {}".format(todo.get("title")))
+
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users"
-    response = requests.get(url)
-    users = response.json()
-    url = "https://jsonplaceholder.typicode.com/todos"
-    response = requests.get(url)
-    todos = response.json()
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({user.get("id"): [{"task": todo.get("title"),
-                                     "completed": todo.get("completed"),
-                                     "username": user.get("username")}
-                                    for todo in todos
-                                    if user.get("id") == todo.get("userId")]
-                  for user in users}, jsonfile)
+    if len(argv) != 2:
+        print("Usage: python3 script.py <user_id>")
+    else:
+        user_id = argv[1]
+        get_todo_list_progress(user_id)

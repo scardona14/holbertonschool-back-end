39fbac1f784c3c5f5c41db276faa858c8d63f538
scardona14 <cardanasean123@gmail.com>
2024-04-16 11:43:03 -0400

    Fixing pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     response = requests.get(url)
     user = response.json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1])
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format
+    (argv[1])
     response = requests.get(url)
     todos = response.json()
     completed = [todo for todo in todos if todo.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                          len(completed), len(todos)))
+                                                          len(completed),
+                                                          len(todos)))
     [print("\t {}".format(todo.get("title"))) for todo in completed]
